89f1c4073e2f76ad5af4769467b95596e1487ab3
applemasterz17 <applemasterz17@gmail.com>
2020-11-23 18:14:33 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// 몽구스 연결 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://applemasterz17:rkdekrxj123@boilerplate.dawdh.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
+}).then(() => console.log('mongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
